4f805019980d4fd8af78d83ce0f1f09820c09f40
rickshaw <rickshawwallis@gmail.com>
2024-09-25 11:53:48 -0300

    s

--- script/pagamento.js
+++ script/pagamento.js
@@ -134,7 +134,7 @@ const enviarMensagemWhatsApp=()=> {
 `;
   
     const codigoPais = '55';
-    const numeroTelefone = '74999508103';
+    const numeroTelefone = '87991614277';
   
     const linkWhatsApp = `https://wa.me/${codigoPais}${numeroTelefone}?text=${encodeURIComponent(textoParaEnviar)}`;
     window.open(linkWhatsApp, '_blank');
